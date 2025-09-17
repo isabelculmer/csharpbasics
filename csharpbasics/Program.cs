a75280281012c7a704180cc4c5926d3561c00f67
imc <imc@L2860>
2025-09-17 09:48:43 +0100

    Add project files.

---
+++ csharpbasics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpbasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+            string name;
+            int number;
+            int number2;
+            number = 6;
+            number2 = 9;
+
+            int answer;
+            answer = number + number2;
+
+
+            Console.WriteLine("What is your age");
+            number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(number);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ switchexample/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string pokemon;
+            //string aboutpokemon = "";
+            //Console.WriteLine("Choose first pokemon");
+            //pokemon = Console.ReadLine();
+
+            //switch (pokemon)
+            //{
+            //    case "Squirtle":
+            //        aboutpokemon = "Type: Water";
+            //        break;
+            //    case "Bulbasaur":
+            //        aboutpokemon = "Type: Grass, Poison";
+            //        break;
+            //    case "Charmander":
+            //        aboutpokemon = "Type: Fire";
+            //        break;
+            //    default:
+            //        aboutpokemon = "not a pokemon";
+            //        break;
+            //}
+
+            //Console.WriteLine(aboutpokemon);
+
+
+            int rating;
+            Console.WriteLine("what is your rating? Out of 5");
+            rating = int.Parse(Console.ReadLine());
+
+            switch (rating)
+            {
+                case 0:
+                    Console.WriteLine("Terrible!!");
+                    Console.WriteLine("worst film ever");
+
+                    break;
+                case 5:
+                    Console.WriteLine("Great!");
+                    break;
+                default:
+                    Console.WriteLine("no rating");
+                    break;
+
+            }
+
+
+        }
+    }
+}
