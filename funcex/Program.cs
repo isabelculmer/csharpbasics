8793af9def8b8c8cf55bcbd228df7a8634f2e9ca
imc <imc@L2860>
2025-11-10 15:08:40 +0000

    Functions

---
+++ funcex/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // double kilo = 0;
+           // kilo =  ;
+
+            List<int> l = new List<int>();
+            l.Add(1);
+            l.Add(2);
+            l.Add(3);
+            Console.WriteLine(Listtotal(l));
+        }
+
+        static int Listtotal(List<int> list)
+        {
+            int total = 0;
+            foreach (int item in list)
+            {
+                total += item;
+            }
+            return total;
+        }
+
+        static bool checkeven(int n)
+        {
+            if (n % 2 == 0)
+            { 
+                return true;
+            }
+            return false;
+        }
+
+
+        static double converttokilo(double miles)
+        {
+            double result = 0;
+            result = miles * 1.6;
+            return result;
+        }
+
+    }
+}
--- listexample/Program.cs
+++ listexample/Program.cs
@@ -10,80 +10,22 @@ namespace list_teaching
     {
         static void Main(string[] args)
         {
-            List<int> mylist = new List<int>();
+          
+            List<int> nums = new List<int>() { 1, 2, 3, 4, 2, 4, 7, 2, 8 };
 
-            mylist.Add(1);
-            mylist.Add(-2);
-            mylist.Add(3);
-
-            mylist.Remove(1);
-            mylist.RemoveAt(0);
-            Example2();
-
-        }
-        static void Example0()//initialising
-        {
-            List<string> names = new List<string>();
-            names.Add("Holly");
-            names.Add("Anika");
-
-            List<string> names2 = new List<string>() { "Holly", "Anika" };
-
-            List<string> names3 = new List<string>();
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < nums.Count; i++)
             {
-                names3.Add(Console.ReadLine());
-            }
-            Display(names);
-        }
-        static void Example1()//split
-        {
-            string sentence = "Hello World My Old Friend";
-            string[] split = sentence.Split(' ');
-            List<string> words = new List<string>(split);
-            //or
-            List<string> words2 = new List<string>();
-            foreach (string word in split)
-            {
-                words2.Add(word);
-            }
-            Display(words);
-        }
-        static void Example2()//removing things from a list
-        {
-            List<int> nums = new List<int>() { 1, 4, 2, 2, 5, 6 };
-            Display(nums);
-            nums.Remove(2);//removes the first instance of the 2 only
-            Console.WriteLine();
-            Display(nums);
-            nums.RemoveAt(nums.Count - 1);// removes the last thing only
-            Display(nums);
-        }
-        static void Example3()
-        {
-            Random random = new Random();
-            List<int> nums = new List<int>() { 1, 3, 2, 6 };
-            //for (int i = 0; i < nums.Count; i++)//this is a BAD way of doing it. don't want to remove items while you are iterating over the list using a for loop
-            //{
-            //    int index = random.Next(nums.Count);
-            //    Console.WriteLine($"Deleting {nums[index]} at {index}");
-            //    nums.RemoveAt(index);
-            //}
-            Display(nums);
-            while (nums.Count > 0)//much better
-            {
-                int index = random.Next(nums.Count);
-                Console.WriteLine($"Deleting {nums[index]} at {index}");
-                nums.RemoveAt(index);
+                for (int j = i + 1; j < nums.Count; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        Console.WriteLine(nums[i]);
+                    }
+                }
             }
 
         }
-        static void Display<T>(List<T> list)//Ignore this. We will explain this much later.
-        {
-            foreach (T item in list)
-            {
-                Console.Write(item + ", ");
-            }
-        }
+       
+        
     }
 }
